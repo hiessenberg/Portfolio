9122b1c9fc03a60edca080771563ddf45254088a
palraj <palrajkonar0@gmail.com>
2025-09-15 13:17:46 +0530

    Updated deployment config

--- Portfolio/jenkins/deployment.tf
+++ Portfolio/jenkins/deployment.tf
@@ -40,12 +40,38 @@ resource "kubernetes_deployment" "jenkins" {
             name       = "jenkins-storage"
             mount_path = "/var/jenkins_home"
           }
+
+          # New: Docker socket mount
+          volume_mount {
+            name       = "docker-sock"
+            mount_path = "/var/run/docker.sock"
+          }
+
+           # New: Kubeconfig mount (for kubectl access)
+          volume_mount {
+            name       = "kube-config"
+            mount_path = "/root/.kube"
+          }
         }
 
         volume {
           name = "jenkins-storage"
           persistent_volume_claim {
             claim_name = kubernetes_persistent_volume_claim.jenkins_pvc.metadata[0].name
+          }
+        }
+       # New: Docker socket
+        volume {
+          name = "docker-sock"
+          host_path {
+            path = "/var/run/docker.sock"
+          }
+        }
+       # New: Kubeconfig
+        volume {
+          name = "kube-config"
+          host_path {
+            path = "/Users/palraj konar/.kube" # replace with your real path
           }
         }
       }
